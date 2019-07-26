444b5ea7a5471c50aa9f3030d0eada36576c4976
Lisa Jensen <leese_38@yahoo.com>
2019-07-26 09:19:42 -0700

    add and name all directories and base files as directed in the prompt, set up files for writing tests

---
+++ WordCounter.Tests/ModelTests/WordCounterTests.cs
@@ -0,0 +1,30 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WordCounter.Models;
+
+namespace WordCounter.Tests
+{
+    [TestClass]
+    public class ClassNameTest
+    {
+        [TestMethod]
+        public void MethodName_Spec_ExpectedOutput()
+        {
+            ClassName instanceName = new ClassName();
+            Assert.AreEqual(ExpectedOutput, instanceName.MethodName(inputHere));
+        }
+
+        [TestMethod]
+        public void MethodName_Spec_ExpectedOutput()
+        {
+            ClassName instanceName = new ClassName();
+            Assert.AreEqual(ExpectedOutput, instanceName.MethodName(inputHere));
+        }
+
+        [TestMethod]
+        public void MethodName_Spec_ExpectedOutput()
+        {
+            ClassName instanceName = new ClassName();
+            Assert.AreEqual(ExpectedOutput, instanceName.MethodName(inputHere));
+        }
+    }
+}
